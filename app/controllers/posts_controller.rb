5e791bd75ae31473af5fd527780606305767544a
gabriel <gabrielnogueira@neemu.com>
2016-11-04 10:26:52 -0200

    clean up code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,39 +1,43 @@
 class PostsController < ApplicationController
-    def index
-      @posts = Post.all
-    end
+  def index
+    @posts = Post.all
+  end
 
-    def new
-      @post = Post.new
-    end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-    def create
-      @post = Post.create(post_params)
-      if @post.save
-        flash[:success] = "Your post has been created."
-        redirect_to posts_path
-      else
-        flash[:alert] = "Halt, you fiend! You need an image to post here!"
-        render :new
-      end
-    end
+  def new
+    @post = Post.new
+  end
 
-    def post_params
-      params.require(:post).permit(:caption, :image)
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      flash[:success] = "Your post has been created."
+      redirect_to posts_path
+    else
+      flash[:alert] = "Halt, you fiend! You need an image to post here!"
+      render :new
     end
+  end
 
-    def show
-      @post = Post.find(params[:id])
-    end
+  def edit
+    @post = Post.find(params[:id])
+  end
 
-    def edit
-      @post = Post.find(params[:id])
-    end
-
-    def update
-      @post = Post.find(params[:id])
-      @post.update(post_params)
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
       flash[:success] = "Post updated hombre."
-      redirect_to(post_path(@post))
+      redirect_to root_path
+    else
+      flash[:alert] = "Something is wrong with your form!"
+      render :edit
     end
+  end
+
+  def post_params
+    params.require(:post).permit(:caption, :image)
+  end
 end
