3bc8e8ef84004dc399715eeb05987e97abc92cfe
gabriel <gabrielnogueira@neemu.com>
2016-11-09 17:39:05 -0200

    fix notification messages to work with comments and likes

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,7 @@
 module NotificationsHelper
+  def notification_message(notification)
+    return "#{notification.notified_by.user_name} #{notification.notice_type}d your post" if notification.notice_type == 'like'
+    "#{notification.notified_by.user_name} #{notification.notice_type}ed on your post"
+
+  end
 end
