d72acda9388cbc41ad24d6d0ae824d298a335412
gabriel <gabrielnogueira@neemu.com>
2016-11-09 18:35:23 -0200

    add follow/unfollow feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'notifications/:id/link_through', to: 'notifications#link_through',
-                                        as: :link_through
-  get 'notifications', to: 'notifications#index'
-
-  get 'profiles/show'
-
-  devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
   resources :posts do
     resources :comments
     member do
@@ -14,9 +8,15 @@ Rails.application.routes.draw do
       get 'unlike'
     end
   end
-
-  root 'posts#index'
+  get 'browse', to: 'posts#browse', as: :browse_posts
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  get 'notifications/:id/link_through', to: 'notifications#link_through',
+                                        as: :link_through
+  get 'notifications', to: 'notifications#index'
+  get 'profiles/show'
   get ':user_name', to: 'profiles#show', as: :profile
   get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
   patch ':user_name/edit', to: 'profiles#update', as: :update_profile
+  post ':user_name/follow_user', to: 'relationships#follow_user', as: :follow_user
+  post ':user_name/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
 end
